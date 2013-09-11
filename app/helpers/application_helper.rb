d6ade58f9c36022074db35059858166fe2a8244d
Rubens Stulzer <rubens@stulzer.com>
2013-09-11 16:03:53 -0300

    Added the https://github.com/fnando/dispatcher-js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,12 @@ module ApplicationHelper
     
     html.html_safe
   end
+
+  def dispatcher_tag
+    controller_name = controller.class.name.underscore
+    controller_name.gsub!(/\//, "_")
+    controller_name.gsub!(/_controller$/, "")
+
+    %[<meta name="page" content="#{controller_name}##{controller.action_name}" />].html_safe
+  end
 end
