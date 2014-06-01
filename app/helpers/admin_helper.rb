35e926429219402a6a769906d48f1473a443e0a4
Rubens Stulzer <rubens@stulzer.com>
2014-06-01 00:17:06 -0300

    Different approach for color and FontAwesome usage.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,11 +1,15 @@
 module AdminHelper
   def link_to_edit(url, html_options = {})
     html_options.reverse_merge!(class: "button-green edit")
-    link_to "Editar", url, html_options
+    link_to url, html_options do
+      icon 'edit', t('edit-button')
+    end
   end
 
   def link_to_destroy(url, html_options = {})
     html_options.reverse_merge!(confirm: 'Tem certeza?', method: :delete, class: "button-red delete")
-    link_to "Deletar", url, html_options
+    link_to url, html_options do
+      icon 'times', t('delete-button')
+    end
   end
 end
