7322e098a6454e3da7a6ae53a45145ab177eafee
Rubens Stulzer <rubens@stulzer.com>
2014-06-15 00:23:26 -0300

    Completely reorganized and "i18ned" the entire app

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,14 +2,14 @@ module AdminHelper
   def link_to_edit(url, html_options = {})
     html_options.reverse_merge!(class: "button-green edit")
     link_to url, html_options do
-      icon 'edit', t('edit-button')
+      icon 'edit', t('edit_button')
     end
   end
 
   def link_to_destroy(url, html_options = {})
     html_options.reverse_merge!(confirm: 'Tem certeza?', method: :delete, class: "button-red delete")
     link_to url, html_options do
-      icon 'times', t('delete-button')
+      icon 'times', t('delete_button')
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,20 +9,18 @@ module ApplicationHelper
     end.join.html_safe
   end
   
-  def analytics(site_id)
+  def analytics(site_id, site_url)
     html = <<-ANALYTICS
     <script>
-     var _gaq = [['_setAccount', '#{site_id}'], ['_trackPageview']];
-     (function(d, t) {
-      var g = d.createElement(t),
-          s = d.getElementsByTagName(t)[0];
-      g.async = true;
-      g.src = '//www.google-analytics.com/ga.js';
-      s.parentNode.insertBefore(g, s);
-     })(document, 'script');
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', '#{site_id}', '#{site_url}');
+      ga('send', 'pageview');
     </script>
     ANALYTICS
-    
     html.html_safe
   end
 
@@ -33,4 +31,9 @@ module ApplicationHelper
 
     content_tag :meta, "", :name => "page", :content => "#{controller_name}##{controller.action_name}".html_safe
   end
+
+  def translate_attribute(model, attribute)
+    model.human_attribute_name(attribute)
+  end
+  alias_method :ta, :translate_attribute
 end
