e805ad71ec5a849590846846d7a4e076147ad0d8
Rubens Stulzer <rubens.junior@vivareal.com>
2015-05-31 01:01:47 -0300

    moves initializer

--- template.rb
+++ template.rb
@@ -8,6 +8,31 @@ end
 
 run "rm Gemfile app/views/layouts/application.html.erb app/helpers/application_helper.rb app/assets/stylesheets/application.css config/locales/en.yml config/database.yml"
 
+initializer 'misc_config.rb', <<-CODE
+module #{Rails.application.class.name.split('::').first.underscore}
+  class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework :rspec, fixture: false, views: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+
+    config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+    config.time_zone = 'Brasilia'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
+
+    config.i18n.enforce_available_locales = false
+
+    config.i18n.available_locales = [:en, :'pt-BR']
+    config.i18n.default_locale = :'pt-BR'
+
+    config.assets.precompile += [ 'html5.js', 'admin/module.js', 'admin/module.css', '.svg', '.eot', '.woff', '.ttf' ]
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  end
+end
+CODE
+
+
 get_file "Gemfile"
 
 # locales
@@ -102,30 +127,6 @@ get_file "app/assets/javascripts/html5.js"
 get_file "app/assets/javascripts/jquery.validate.js"
 run "mkdir -p app/assets/javascripts/validate/localization"
 get_file "app/assets/javascripts/validate/localization/messages_pt_BR.js"
-
-initializer 'misc_config.rb', <<-CODE
-module #{Rails.application.class.name.split('::').first.underscore}
-  class Application < Rails::Application
-    config.generators do |g|
-      g.test_framework :rspec, fixture: false, views: false
-      g.fixture_replacement :factory_girl, dir: 'spec/factories'
-    end
-
-    config.action_mailer.default_url_options = { host: 'localhost:3000' }
-
-    config.time_zone = 'Brasilia'
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
-
-    config.i18n.enforce_available_locales = false
-
-    config.i18n.available_locales = [:en, :'pt-BR']
-    config.i18n.default_locale = :'pt-BR'
-
-    config.assets.precompile += [ 'html5.js', 'admin/module.js', 'admin/module.css', '.svg', '.eot', '.woff', '.ttf' ]
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-  end
-end
-CODE
 
 # bundling
 run "bundle install"
