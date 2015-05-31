56cc6757a0e6fdc5e5678b11e72c9cbf0e98eb11
Rubens Stulzer <rubens.junior@vivareal.com>
2015-05-31 03:03:24 -0300

    improves readability

--- template.rb
+++ template.rb
@@ -3,10 +3,10 @@ def get_file(file)
 end
 
 def template_url
-  "https://github.com/stulzer/rails-template/raw/master"
+  'https://github.com/stulzer/rails-template/raw/master'
 end
 
-run "rm Gemfile app/views/layouts/application.html.erb app/helpers/application_helper.rb app/assets/stylesheets/application.css config/locales/en.yml config/database.yml"
+run 'rm Gemfile app/views/layouts/application.html.erb app/helpers/application_helper.rb app/assets/stylesheets/application.css config/locales/en.yml config/database.yml'
 
 initializer 'generators.rb', <<-CODE
 module #{app_name.camelize}
@@ -47,112 +47,118 @@ module #{app_name.camelize}
 end
 CODE
 
-get_file "Gemfile"
+get_file 'Gemfile'
 
 # locales
-get_file "config/locales/en/rails.yml"
-get_file "config/locales/en/admin.yml"
-get_file "config/locales/en/app.yml"
-get_file "config/locales/en/devise.views.yml"
-get_file "config/locales/en/devise.yml"
+get_file 'config/locales/en/rails.yml'
+get_file 'config/locales/en/admin.yml'
+get_file 'config/locales/en/app.yml'
+get_file 'config/locales/en/devise.views.yml'
+get_file 'config/locales/en/devise.yml'
 
-get_file "config/locales/pt-BR/rails.yml"
-get_file "config/locales/pt-BR/admin.yml"
-get_file "config/locales/pt-BR/app.yml"
-get_file "config/locales/pt-BR/devise.views.yml"
-get_file "config/locales/pt-BR/devise.yml"
+get_file 'config/locales/pt-BR/rails.yml'
+get_file 'config/locales/pt-BR/admin.yml'
+get_file 'config/locales/pt-BR/app.yml'
+get_file 'config/locales/pt-BR/devise.views.yml'
+get_file 'config/locales/pt-BR/devise.yml'
 
 # application layout and helper
-get_file "app/views/layouts/application.html.erb"
-get_file "app/helpers/application_helper.rb"
+get_file 'app/views/layouts/application.html.erb'
+get_file 'app/helpers/application_helper.rb'
 
 # error_messages_for
-run "mkdir -p app/views/shared"
-get_file "app/views/shared/_error_messages.html.erb"
+run 'mkdir -p app/views/shared'
+get_file 'app/views/shared/_error_messages.html.erb'
 
 # basic sass files
-get_file "app/assets/stylesheets/application/misc/_normalize.scss"
-get_file "app/assets/stylesheets/application/misc/_functions.scss"
-get_file "app/assets/stylesheets/application/misc/_mixins.scss"
-get_file "app/assets/stylesheets/application.css"
-get_file "app/assets/stylesheets/application/imports.scss"
+get_file 'app/assets/stylesheets/application/misc/_normalize.scss'
+get_file 'app/assets/stylesheets/application/misc/_functions.scss'
+get_file 'app/assets/stylesheets/application/misc/_mixins.scss'
+get_file 'app/assets/stylesheets/application.css'
+get_file 'app/assets/stylesheets/application/imports.scss'
 
 # basic admin assets
-get_file "app/assets/stylesheets/admin/module.css.scss"
-get_file "app/assets/stylesheets/admin/misc/_vars.scss"
-get_file "app/assets/stylesheets/admin/misc/_normalize.scss"
-get_file "app/assets/stylesheets/admin/ui/_buttons.scss"
-get_file "app/assets/stylesheets/admin/ui/_confirmable.scss"
-get_file "app/assets/stylesheets/admin/ui/_forms.scss"
-get_file "app/assets/stylesheets/admin/ui/_header.scss"
-get_file "app/assets/stylesheets/admin/ui/_index.scss"
-get_file "app/assets/stylesheets/admin/ui/_markdown.scss"
-get_file "app/assets/stylesheets/admin/ui/_nav.scss"
-get_file "app/assets/stylesheets/admin/vendor/_avgrund.scss"
-get_file "app/assets/stylesheets/admin/vendor/_meny.scss"
-get_file "app/assets/javascripts/admin/avgrund.js"
-get_file "app/assets/javascripts/admin/bootstrap-markdown.js"
+get_file 'app/assets/stylesheets/admin/module.css.scss'
+get_file 'app/assets/stylesheets/admin/misc/_vars.scss'
+get_file 'app/assets/stylesheets/admin/misc/_normalize.scss'
+get_file 'app/assets/stylesheets/admin/ui/_buttons.scss'
+get_file 'app/assets/stylesheets/admin/ui/_confirmable.scss'
+get_file 'app/assets/stylesheets/admin/ui/_forms.scss'
+get_file 'app/assets/stylesheets/admin/ui/_header.scss'
+get_file 'app/assets/stylesheets/admin/ui/_index.scss'
+get_file 'app/assets/stylesheets/admin/ui/_markdown.scss'
+get_file 'app/assets/stylesheets/admin/ui/_nav.scss'
+get_file 'app/assets/stylesheets/admin/vendor/_avgrund.scss'
+get_file 'app/assets/stylesheets/admin/vendor/_meny.scss'
+get_file 'app/assets/javascripts/admin/avgrund.js'
+get_file 'app/assets/javascripts/admin/bootstrap-markdown.js'
 
 # basic admin views
-run "mkdir -p app/views/admin app/views/admin/passwords app/views/admin/sessions app/views/admin/admins app/views/admin/shared app/views/devise/mailer"
-get_file "app/views/admin/passwords/edit.html.erb"
-get_file "app/views/admin/passwords/new.html.erb"
-get_file "app/views/admin/sessions/new.html.erb"
-get_file "app/views/admin/shared/_links.erb"
-get_file "app/views/admin/shared/_modal_box.html.erb"
-get_file "app/views/devise/mailer/reset_password_instructions.html.erb"
+run 'mkdir -p app/views/admin app/views/admin/passwords app/views/admin/sessions app/views/admin/admins app/views/admin/shared app/views/devise/mailer'
+get_file 'app/views/admin/passwords/edit.html.erb'
+get_file 'app/views/admin/passwords/new.html.erb'
+get_file 'app/views/admin/sessions/new.html.erb'
+get_file 'app/views/admin/shared/_links.erb'
+get_file 'app/views/admin/shared/_modal_box.html.erb'
+get_file 'app/views/devise/mailer/reset_password_instructions.html.erb'
 
-get_file "app/assets/javascripts/admin/module.js"
-get_file "app/assets/javascripts/admin/meny.js"
-get_file "app/views/layouts/admin.html.erb"
+get_file 'app/assets/javascripts/admin/module.js'
+get_file 'app/assets/javascripts/admin/meny.js'
+get_file 'app/views/layouts/admin.html.erb'
 
 # basic db configuration
-get_file "config/database.yml"
+get_file 'config/database.yml'
 
 # basic admin controllers
-run "mkdir -p app/controllers/admin"
-get_file "app/controllers/admin/base_controller.rb"
-get_file "app/controllers/admin/passwords_controller.rb"
-get_file "app/controllers/admin/sessions_controller.rb"
-get_file "app/controllers/admin/unlocks_controller.rb"
+run 'mkdir -p app/controllers/admin'
+get_file 'app/controllers/admin/base_controller.rb'
+get_file 'app/controllers/admin/passwords_controller.rb'
+get_file 'app/controllers/admin/sessions_controller.rb'
+get_file 'app/controllers/admin/unlocks_controller.rb'
 
 # basic admin views
-get_file "app/controllers/admin/admins_controller.rb"
+get_file 'app/controllers/admin/admins_controller.rb'
 
-get_file "app/views/admin/admins/index.html.erb"
-get_file "app/views/admin/admins/show.html.erb"
-get_file "app/views/admin/admins/new.html.erb"
-get_file "app/views/admin/admins/edit.html.erb"
-get_file "app/views/admin/admins/_form.html.erb"
-get_file "app/views/admin/admins/destroy.js.erb"
-get_file "app/views/admin/admins/confirm_destroy.js.erb"
+get_file 'app/views/admin/admins/index.html.erb'
+get_file 'app/views/admin/admins/show.html.erb'
+get_file 'app/views/admin/admins/new.html.erb'
+get_file 'app/views/admin/admins/edit.html.erb'
+get_file 'app/views/admin/admins/_form.html.erb'
+get_file 'app/views/admin/admins/destroy.js.erb'
+get_file 'app/views/admin/admins/confirm_destroy.js.erb'
 
 # admin helper
-get_file "app/helpers/admin_helper.rb"
+get_file 'app/helpers/admin_helper.rb'
 
 # basic js files
-run "rm app/assets/javascripts/application.js"
-get_file "app/assets/javascripts/application.js"
-get_file "app/assets/javascripts/dispatcher.js"
-get_file "app/assets/javascripts/jquery.validate.js"
-run "mkdir -p app/assets/javascripts/validate/localization"
-get_file "app/assets/javascripts/validate/localization/messages_pt_BR.js"
+run 'rm app/assets/javascripts/application.js'
+get_file 'app/assets/javascripts/application.js'
+get_file 'app/assets/javascripts/dispatcher.js'
+get_file 'app/assets/javascripts/jquery.validate.js'
+run 'mkdir -p app/assets/javascripts/validate/localization'
+get_file 'app/assets/javascripts/validate/localization/messages_pt_BR.js'
 
 # bundling
-run "bundle install"
+run 'bundle install'
 
 # rspec
-generate "rspec:install"
+generate 'rspec:install'
 
 # devise
-generate "devise:install"
+generate 'devise:install'
 
 # devise
-generate "devise admin"
+generate 'devise admin'
+
+
+inject_into_file 'name_of_file.rb', after: "#The code goes below this line. Don't forget the Line break at the end\n" do <<-'RUBY'
+  puts "Hello World"
+RUBY
+end
 
 # admin routes
-inject_into_file "config/routes.rb",
-  ", controllers: {
+inject_into_file 'config/routes.rb', after: 'devise_for :admins' do <<-'RUBY'
+  , controllers: {
     sessions:  'admin/sessions',
     passwords: 'admin/passwords',
     unlocks:   'admin/unlocks'
@@ -165,36 +171,37 @@ inject_into_file "config/routes.rb",
       end
     end
     root to: 'admins#index'
-  end",
-  after: "devise_for :admins"
+  end
+RUBY
+end
 
 # adding html responder
-inject_into_file "app/controllers/application_controller.rb",
-  "\n\n  respond_to :html",
-  after: "protect_from_forgery with: :exception"
+inject_into_file 'app/controllers/application_controller.rb',
+  '  respond_to :html',
+  after: 'protect_from_forgery with: :exception'
 
 # Improve README
-get_file "README.md_example"
-run "mv README.md_example README.md"
-run "rm README.rdoc"
+get_file 'README.md_example'
+run 'mv README.md_example README.md'
+run 'rm README.rdoc'
 
 # init guard
-run "bundle exec guard init livereload"
+run 'bundle exec guard init livereload'
 
-run "mv spec/spec_helper.rb spec/.spec_helper_backup"
-get_file "spec/spec_helper.rb"
+run 'mv spec/spec_helper.rb spec/.spec_helper_backup'
+get_file 'spec/spec_helper.rb'
 
-run "mv spec/rails_helper.rb spec/.rails_helper_backup"
-get_file "spec/rails_helper.rb"
+run 'mv spec/rails_helper.rb spec/.rails_helper_backup'
+get_file 'spec/rails_helper.rb'
 
-generate "migration add_name_to_admins name"
+generate 'migration add_name_to_admins name'
 
-run "bundle binstubs rspec-core"
-run "bundle binstubs guard"
+run 'bundle binstubs rspec-core'
+run 'bundle binstubs guard'
 
 # git
 git :init
 
-puts "=================================="
-puts "CONFIGURE THE DATABASE.YML AND MIGRATE"
-puts "CHECK SPECS HELPERS spec/rails_helper.rb spec/rails_helper.rb"
+puts '=================================='
+puts 'CONFIGURE THE DATABASE.YML AND MIGRATE'
+puts 'CHECK SPECS HELPERS spec/rails_helper.rb spec/rails_helper.rb'
